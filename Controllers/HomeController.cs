8c2213be5a98ba027855cbc8855d47fc6773816a
Markiian <pshchuplyak@gmail.com>
2022-03-20 20:22:13 +0200

    [NF]: Add connection string

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 using WEB_Basics_Project.Models;
 
@@ -13,11 +8,9 @@ namespace WEB_Basics_Project.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
---
+++ Data/Models/DbSettings.cs
@@ -0,0 +1,9 @@
+﻿using WEB_Basics_Project.Repository;
+
+namespace WEB_Basics_Project.Data.Models
+{
+    public class DbSettings : IDbSettings
+    {
+        public string ConnectionString { get; set; }
+    }
+}
