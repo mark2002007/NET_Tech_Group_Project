a0921be2f28a2e85a740f188e9a93e4819ede995
Маркіян Мандзак <markmandzak2002@gmail.com>
2022-05-11 12:39:57 +0300

    Logic Separation -Separated DB Acess Logic New Project -Fixed Namespace Error -FIxed Migrations

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace WEB_Basics_Project.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Models/Volunteer.cs
@@ -0,0 +1,17 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace WEB_Basics_Project.Models
+{
+    public class Volunteer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int VolunteerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public List<string> Position { get; set; }
+    }
+}
